ee9a1362949637384a28b5a802f6a0d5c8615d05
kiritoEM <johankirito64@gmail.com>
2023-12-16 11:02:49 +0100

    add testimonial buttons

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,11 +1,13 @@
 import TestimonialsContent from "./Testimonials/TestimonialsContent";
 import TestimonialsHeader from "./Testimonials/TestimonialsHeader";
+import TestimonialsButtons from "./Testimonials/TestimonialsButtons";
 
 const Testimonials = (): JSX.Element => {
   return (
     <section id="testimonials">
       <TestimonialsHeader />
       <TestimonialsContent />
+      <TestimonialsButtons />
     </section>
   );
 };
---
+++ src/components/Testimonials/TestimonialsButtons.tsx
@@ -0,0 +1,14 @@
+const TestimonialsButtons = (): JSX.Element => {
+  return (
+    <div className="testimonials-buttons">
+      <div className="arraw-left">
+        <img src="/images/testimonials/Arrow - Left.png" alt="" />
+      </div>
+      <div className="arraw-right">
+        <img src="/images/testimonials/Arrow - Right.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsButtons;
