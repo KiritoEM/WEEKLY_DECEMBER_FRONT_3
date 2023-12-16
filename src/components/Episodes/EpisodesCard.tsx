bb6d559d967b4c5c5f3d7f19cf979a87b4365593
kiritoEM <johankirito64@gmail.com>
2023-12-16 14:27:02 +0100

    add card to episodes

--- src/components/Episodes/EpisodesCard.tsx
+++ src/components/Episodes/EpisodesCard.tsx
@@ -1,21 +1,32 @@
 const EpisodesCard = (): JSX.Element => {
   return (
     <div className="episodes-card">
-      <div className="cover">
-        <img src="/images/episodes/episode-1.png" alt="" />
+      <div className="first-section">
+        <div className="cover">
+          <img src="/images/episodes/episode-1.png" alt="" />
+        </div>
+        <div className="header">
+          <div className="title">
+            <h6>Eps. 6</h6>
+            <h5>Pandemic Becoming Endemic</h5>
+          </div>
+          <div className="line"></div>
+          <div className="text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              ac ultrices odio.{" "}
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="header">
-        <div className="title">
-          <h6>Eps. 6</h6>
-          <h5>Pandemic Becoming Endemic</h5>
-        </div>
-        <div className="line"></div>
-        <div className="text">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            ac ultrices odio.{" "}
-          </p>
-        </div>
+      <div className="second-section">
+          <div className="tags">
+              <button className="btn">Covid19</button>
+              <button className="btn">Health</button>
+          </div>
+          <div className="hosted">
+               <p>Hosted by: <img src="/images/episodes/host.png" alt="" /></p>
+          </div>
       </div>
     </div>
   );
