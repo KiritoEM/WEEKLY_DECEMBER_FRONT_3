588b6da566b4bd4eebf3c44f9084841eb3ed2eff
kiritoEM <johankirito64@gmail.com>
2023-12-17 08:41:30 +0100

    add shape to blog

--- src/components/blog/Blogcontent.tsx
+++ src/components/blog/Blogcontent.tsx
@@ -5,12 +5,18 @@ const Blogcontent = (): JSX.Element => {
   const { blog } = blogData();
   return (
     <div className="section-content">
+      <div className="shape-1">
+        <img src="/images/blog/shape-1.png" alt="" />
+      </div>
       <div className="row">
         {blog.map((item, index) => (
           <div className="col-6" key={index}>
             <BlogCard {...item} />
           </div>
         ))}
+      </div>
+      <div className="shape-2">
+        <img src="/images/blog/shape-2.png" alt="" />
       </div>
     </div>
   );
